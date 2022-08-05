b8e4683c9c6728abc0be068eeb3f305ee7ddcb4c
Kylejeong2 <Kylejeong21@gmail.com>
2022-08-05 11:27:06 -0700

    project upload

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { KeyboardAvoidingView, Platform } from "react-native";
+import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
+import EatsScreen from "./screens/EatsScreen";
+import { store } from "./store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <KeyboardAvoidingView 
+          style={{ flex: 1 }}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+          >
+            <Stack.Navigator>
+              <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="MapScreen"
+                component={MapScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="EatsScreen"
+                component={EatsScreen}
+                options={{
+                  headerShown: false
+                }}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/EatsScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import tw from "tailwind-react-native-classnames";
+
+const EatsScreen = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+        <Text style={tw`text-center bg-gray-300`}>EatScreen Coming Soon ... </Text> 
+    </SafeAreaView>
+  );
+};
+
+export default EatsScreen;
+
+const styles = StyleSheet.create({});
